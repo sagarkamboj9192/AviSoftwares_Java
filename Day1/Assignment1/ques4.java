411b1f5095557a67e7435dea312d73458dace79e
unknown <kambojsagar4843@gmail.com>
2024-01-29 11:29:00 +0530

    Second Commit

--- Day1/Assignment1/ques4.java
+++ Day1/Assignment1/ques4.java
@@ -1,26 +1,37 @@
-import java.util.Vector;
+import java.util.*;
 
 public class ques4 {
     public static void main(String[] pra)
     {
-        int n=12;
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter the number to calculate factor: ");
+        int n = obj.nextInt();
+
         if(n<4){
             System.out.println(-1);
         }
+        else{
+            int val = (int) Math.sqrt(n);
+            Vector<Integer> vec = new Vector<Integer>();
+            for(int i=2;i<=val;i++){
+                if(n%i == 0 ){
+                    int ans=(int) n/i;
+                    vec.add(i);
+                    if(ans!= i){
+                        vec.add(ans);
+                    }
+                    
+                }
+            }
 
-        Vector<Integer> vec = new Vector<Integer>();
-
-        for(int i=2;i<=n/2;i++){
-            if(n%i == 0){
-                vec.add(i);
+            if(vec.size() > 0){
+                System.out.println(vec);
+            }
+            else{
+                System.out.println(-1);
             }
         }
-
-        if(vec.size() > 0){
-            System.out.println(vec);
-        }
-        else{
-            System.out.println(-1);
-        }
+        obj.close();
+        
     }
 }
